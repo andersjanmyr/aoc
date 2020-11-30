148b324f825f84a0e0d2ca512483081620c11c00
Anders Janmyr <anders@janmyr.com>
2020-11-30 14:43:52 +0100

    Added matrix functions

--- 2020/01/src/lib.rs
+++ 2020/01/src/lib.rs
@@ -2,23 +2,35 @@ use std::fs::File;
 use std::io::{self, prelude::*};
 
 pub fn main() {
-    println!("{:?}", numbers());
+    println!("{:?}", num_matrix());
 }
 
 fn numbers() -> Vec<i32> {
     let lines = strings();
+    lines.iter().map(|s| s.parse::<i32>().unwrap()).collect()
+}
+
+fn num_matrix() -> Vec<Vec<i32>> {
+    let lines = matrix();
     lines
         .iter()
-        .filter(|s| s.parse::<i32>().is_ok())
-        .map(|s| s.parse::<i32>().unwrap())
+        .map(|ss| ss.iter().map(|s| s.parse::<i32>().unwrap()).collect())
+        .collect()
+}
+
+fn matrix() -> Vec<Vec<String>> {
+    let lines = strings();
+    lines
+        .iter()
+        .map(|l| l.split(",").map(|s| s.to_string()).collect())
         .collect()
 }
 
 fn strings() -> Vec<String> {
     let lines = read_lines();
     match lines {
-        Ok(lines) => lines.map(|l| l.unwrap()).collect(),
-        Err(_) => Vec::new(),
+        Ok(lines) => lines.map(|l| l.unwrap()).filter(|l| l != "").collect(),
+        Err(err) => panic!(err),
     }
 }
 
@@ -33,8 +45,13 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_numbers() {
-        let ns = numbers();
+    fn test_matrix() {
+        let ns = matrix();
         assert_eq!(ns.len(), 4);
+    }
+
+    #[test]
+    fn test_main() {
+        main();
     }
 }
