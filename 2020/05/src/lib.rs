a9cbe3b5f28662276cb5dab605cf80f01b1126a1
Anders Janmyr <anders@janmyr.com>
2020-12-05 08:22:43 +0100

    Solved 5

--- 2020/05/src/lib.rs
+++ 2020/05/src/lib.rs
@@ -1,116 +1,49 @@
-use regex::Regex;
-use std::fmt::Debug;
 use std::fs::File;
 use std::io::{self, prelude::*};
 
 pub fn main() {
-    let ps = passports();
-    let valids: Vec<Passport> = ps.into_iter().filter(|p| p.is_valid()).collect();
-    println!("{:?}, {:?}", valids, valids.len());
-}
-
-// ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
-// byr:1937 iyr:2017 cid:147 hgt:183cm
-#[derive(Debug, Clone)]
-pub struct Passport {
-    ecl: String,
-    pid: String,
-    eyr: String,
-    hcl: String,
-    byr: String,
-    iyr: String,
-    cid: String,
-    hgt: String,
-}
-
-fn get(s: &str, name: &str) -> String {
-    let pairs = s.split(" ");
-    for p in pairs {
-        let v: Vec<&str> = p.split(":").collect();
-        if v[0] == name {
-            return v[1].to_string();
-        }
-    }
-    return "".to_string();
-}
-
-impl Passport {
-    pub fn new(pairs: &str) -> Self {
-        println!("{:?}", pairs);
-        let p = Passport {
-            ecl: get(pairs, "ecl"),
-            pid: get(pairs, "pid"),
-            eyr: get(pairs, "eyr"),
-            hcl: get(pairs, "hcl"),
-            byr: get(pairs, "byr"),
-            iyr: get(pairs, "iyr"),
-            cid: get(pairs, "cid"),
-            hgt: get(pairs, "hgt"),
-        };
-        p
-    }
-    pub fn is_valid(&self) -> bool {
-        Self::is_valid_num(&self.byr, 1920, 2020)
-            && Self::is_valid_num(&self.iyr, 2010, 2020)
-            && Self::is_valid_num(&self.eyr, 2020, 2030)
-            && self.is_valid_hgt()
-            && self.is_valid_hcl()
-            && self.is_valid_ecl()
-            && self.is_valid_pid()
-    }
-
-    pub fn is_valid_num(s: &str, min: i32, max: i32) -> bool {
-        match s.parse::<i32>() {
-            Ok(i) => i >= min && i <= max,
-            Err(_) => false,
-        }
-    }
-
-    pub fn is_valid_hgt(&self) -> bool {
-        if self.hgt == "" {
-            return false;
-        }
-        let len = self.hgt.len() - 2;
-        if self.hgt.ends_with("in") {
-            Self::is_valid_num(&self.hgt[..len], 59, 76)
-        } else if self.hgt.ends_with("cm") {
-            Self::is_valid_num(&self.hgt[..len], 150, 193)
-        } else {
-            false
-        }
-    }
-
-    pub fn is_valid_hcl(&self) -> bool {
-        let re = Regex::new(r"^#[0-9a-f]{6}$").unwrap();
-        re.is_match(&self.hcl)
-    }
-
-    pub fn is_valid_ecl(&self) -> bool {
-        let re = Regex::new(r"^(amb)|(blu)|(brn)|(gry)|(grn)|(hzl)|(oth)$").unwrap();
-        re.is_match(&self.ecl)
-    }
-    pub fn is_valid_pid(&self) -> bool {
-        let re = Regex::new(r"^[0-9]{9}$").unwrap();
-        re.is_match(&self.pid)
-    }
-}
-
-fn passports() -> Vec<Passport> {
-    let mut ps = Vec::new();
     let ls = strings();
-    let mut pairs = "".to_string();
+
+    let mut max = 0;
+    let mut seats = Vec::new();
     for l in ls {
-        if l == "" {
-            let p = Passport::new(&pairs);
-            ps.push(p);
-            pairs = "".to_string();
+        let (r, c, id) = calc_seat(&l);
+        seats.push(id);
+        if id > max {
+            max = id;
         }
-        pairs.push_str(" ");
-        pairs.push_str(&l);
     }
-    let p = Passport::new(&pairs);
-    ps.push(p);
-    ps
+    println!("MAX: {:?}", max);
+    seats.sort();
+    for i in 0..seats.len() - 1 {
+        if seats[i] + 1 != seats[i + 1] {
+            println!("Seat: {:?}", seats[i] + 1);
+        }
+    }
+}
+
+fn calc_seat(s: &str) -> (i32, i32, i32) {
+    let r = bin_part(0, 127, &s[0..7]);
+    let c = bin_part(0, 7, &s[7..]);
+    (r, c, r * 8 + c)
+}
+
+fn bin_part(low: i32, high: i32, s: &str) -> i32 {
+    let (l, _) = s.chars().fold((low, high), |(l, h), c| match c {
+        'F' | 'L' => next_range(l, h, true),
+        'B' | 'R' => next_range(l, h, false),
+        _ => panic!(""),
+    });
+    l
+}
+
+fn next_range(low: i32, high: i32, choose_low: bool) -> (i32, i32) {
+    let mid = (low + high) / 2;
+    if choose_low {
+        (low, mid)
+    } else {
+        (mid + 1, high)
+    }
 }
 
 fn numbers() -> Vec<i32> {
@@ -154,9 +87,20 @@ mod tests {
 
     #[test]
     #[ignore]
-    fn test_matrix() {
-        let ns = numbers();
-        assert_eq!(ns.len(), 4);
+    fn test_bin_part() {
+        let l = bin_part(0, 127, "FBFBBFF");
+        assert_eq!(l, 44);
+        let l = bin_part(0, 7, "RLR");
+        assert_eq!(l, 5);
+    }
+
+    #[test]
+    #[ignore]
+    fn test_range() {
+        let (l, p) = next_range(0, 127, false);
+        assert_eq!((l, p), (64, 127));
+        let (l, p) = next_range(0, 127, true);
+        assert_eq!((l, p), (0, 63));
     }
 
     #[test]
