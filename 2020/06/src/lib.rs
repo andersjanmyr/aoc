42e6143ddcc77b4d374f9867cdebcc40bca2f1aa
Anders Janmyr <anders@janmyr.com>
2020-12-06 08:38:05 +0100

    Solved six

--- 2020/06/src/lib.rs
+++ 2020/06/src/lib.rs
@@ -1,49 +1,37 @@
+use std::collections::HashSet;
 use std::fs::File;
 use std::io::{self, prelude::*};
+use std::iter::FromIterator;
 
 pub fn main() {
+    let gs = groups();
+    let sum: usize = gs.iter().map(|g| count(g)).sum();
+    println!("{:?} {:?}", gs, sum);
+}
+
+fn count(v: &Vec<String>) -> usize {
+    let mut set: HashSet<char> = HashSet::from_iter(v[0].chars());
+    for i in 1..v.len() {
+        let new_set = HashSet::from_iter(v[i].chars());
+        set = HashSet::from_iter(set.intersection(&new_set).cloned());
+    }
+    set.len()
+}
+
+fn groups() -> Vec<Vec<String>> {
+    let mut gs = Vec::new();
     let ls = strings();
-
-    let mut max = 0;
-    let mut seats = Vec::new();
+    let mut group: Vec<String> = Vec::new();
     for l in ls {
-        let (r, c, id) = calc_seat(&l);
-        seats.push(id);
-        if id > max {
-            max = id;
+        if l == "" {
+            gs.push(group);
+            group = Vec::new();
+        } else {
+            group.push(l);
         }
     }
-    println!("MAX: {:?}", max);
-    seats.sort();
-    for i in 0..seats.len() - 1 {
-        if seats[i] + 1 != seats[i + 1] {
-            println!("Seat: {:?}", seats[i] + 1);
-        }
-    }
-}
-
-fn calc_seat(s: &str) -> (i32, i32, i32) {
-    let r = bin_part(0, 127, &s[0..7]);
-    let c = bin_part(0, 7, &s[7..]);
-    (r, c, r * 8 + c)
-}
-
-fn bin_part(low: i32, high: i32, s: &str) -> i32 {
-    let (l, _) = s.chars().fold((low, high), |(l, h), c| match c {
-        'F' | 'L' => next_range(l, h, true),
-        'B' | 'R' => next_range(l, h, false),
-        _ => panic!(""),
-    });
-    l
-}
-
-fn next_range(low: i32, high: i32, choose_low: bool) -> (i32, i32) {
-    let mid = (low + high) / 2;
-    if choose_low {
-        (low, mid)
-    } else {
-        (mid + 1, high)
-    }
+    gs.push(group);
+    gs
 }
 
 fn numbers() -> Vec<i32> {
@@ -87,21 +75,11 @@ mod tests {
 
     #[test]
     #[ignore]
-    fn test_bin_part() {
-        let l = bin_part(0, 127, "FBFBBFF");
-        assert_eq!(l, 44);
-        let l = bin_part(0, 7, "RLR");
-        assert_eq!(l, 5);
-    }
+    fn test_bin_part() {}
 
     #[test]
     #[ignore]
-    fn test_range() {
-        let (l, p) = next_range(0, 127, false);
-        assert_eq!((l, p), (64, 127));
-        let (l, p) = next_range(0, 127, true);
-        assert_eq!((l, p), (0, 63));
-    }
+    fn test_range() {}
 
     #[test]
     fn test_main() {
