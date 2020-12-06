650a214afab5495e05296d2c995f03eb22bd29ff
Anders Janmyr <anders@janmyr.com>
2020-12-06 08:38:59 +0100

    Added seven

---
+++ 2020/07/src/lib.rs
@@ -0,0 +1,88 @@
+use std::collections::HashSet;
+use std::fs::File;
+use std::io::{self, prelude::*};
+use std::iter::FromIterator;
+
+pub fn main() {
+    let gs = groups();
+    let sum: usize = gs.iter().map(|g| count(g)).sum();
+    println!("{:?} {:?}", gs, sum);
+}
+
+fn count(v: &Vec<String>) -> usize {
+    let mut set: HashSet<char> = HashSet::from_iter(v[0].chars());
+    for i in 1..v.len() {
+        let new_set = HashSet::from_iter(v[i].chars());
+        set = HashSet::from_iter(set.intersection(&new_set).cloned());
+    }
+    set.len()
+}
+
+fn groups() -> Vec<Vec<String>> {
+    let mut gs = Vec::new();
+    let ls = strings();
+    let mut group: Vec<String> = Vec::new();
+    for l in ls {
+        if l == "" {
+            gs.push(group);
+            group = Vec::new();
+        } else {
+            group.push(l);
+        }
+    }
+    gs.push(group);
+    gs
+}
+
+fn numbers() -> Vec<i32> {
+    let lines = strings();
+    lines.iter().map(|s| s.parse::<i32>().unwrap()).collect()
+}
+
+fn num_matrix() -> Vec<Vec<i32>> {
+    let lines = matrix();
+    lines
+        .iter()
+        .map(|ss| ss.iter().map(|s| s.parse::<i32>().unwrap()).collect())
+        .collect()
+}
+
+fn matrix() -> Vec<Vec<String>> {
+    let lines = strings();
+    lines
+        .iter()
+        .map(|l| l.split(",").map(|s| s.to_string()).collect())
+        .collect()
+}
+
+fn strings() -> Vec<String> {
+    let lines = read_lines();
+    match lines {
+        Ok(lines) => lines.map(|l| l.unwrap()).collect(),
+        Err(err) => panic!(err),
+    }
+}
+
+fn read_lines() -> io::Result<io::Lines<io::BufReader<File>>> {
+    let file = File::open("input.txt")?;
+    let reader = io::BufReader::new(file);
+    Ok(reader.lines())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    #[ignore]
+    fn test_bin_part() {}
+
+    #[test]
+    #[ignore]
+    fn test_range() {}
+
+    #[test]
+    fn test_main() {
+        main();
+    }
+}
