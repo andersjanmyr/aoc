683abeb090c095b441a2a2c75582a2cec18045ca
Anders Janmyr <anders@janmyr.com>
2020-12-07 10:57:17 +0100

    Solved 7

--- 2020/07/src/lib.rs
+++ 2020/07/src/lib.rs
@@ -1,21 +1,81 @@
 use std::collections::HashSet;
 use std::fs::File;
 use std::io::{self, prelude::*};
-use std::iter::FromIterator;
 
-pub fn main() {
-    let gs = groups();
-    let sum: usize = gs.iter().map(|g| count(g)).sum();
-    println!("{:?} {:?}", gs, sum);
+#[derive(Debug)]
+pub struct Rule {
+    name: String,
+    has: Vec<ContainedRule>,
 }
 
-fn count(v: &Vec<String>) -> usize {
-    let mut set: HashSet<char> = HashSet::from_iter(v[0].chars());
-    for i in 1..v.len() {
-        let new_set = HashSet::from_iter(v[i].chars());
-        set = HashSet::from_iter(set.intersection(&new_set).cloned());
+#[derive(Debug)]
+pub struct ContainedRule {
+    count: i32,
+    name: String,
+}
+
+pub fn main() {
+    let rules = parse_rules();
+    println!("{:?}", rules);
+    // let matching = traverse(&rules, "shiny gold");
+    // println!("{:?} {:?}", matching, matching.len());
+    let count = traverse2("shiny gold", &rules);
+    println!("{:?}", count - 1);
+}
+
+fn parse_rules() -> Vec<Rule> {
+    let ls = strings();
+    ls.iter().map(|l| parse_line(&l)).collect()
+}
+
+fn parse_line(l: &str) -> Rule {
+    let mut s = l.replace(",", "");
+    s = s.replace(".", "");
+    let parts: Vec<&str> = s.split(" ").collect();
+    let name = format!("{} {}", parts[0], parts[1]);
+    let mut has = Vec::new();
+    for i in (4..parts.len()).step_by(4) {
+        if parts[i] != "no" {
+            let cr = ContainedRule {
+                count: parts[i].parse::<i32>().unwrap(),
+                name: format!("{} {}", parts[i + 1], parts[i + 2]),
+            };
+            has.push(cr);
+        }
     }
-    set.len()
+    Rule {
+        name: name,
+        has: has,
+    }
+}
+
+fn traverse(rules: &Vec<Rule>, bag: &str) -> Vec<String> {
+    let mut set = HashSet::new();
+    set.insert(bag.to_string());
+    let mut len = 0;
+    while len < set.len() {
+        len = set.len();
+        for r in rules {
+            println!("{:?}", r);
+            for cr in &r.has {
+                if set.contains(&cr.name) {
+                    println!("{:?}", cr.name);
+                    set.insert(r.name.to_string());
+                }
+            }
+        }
+        println!("{:?}\n", set);
+    }
+    set.remove(bag);
+    set.into_iter().collect()
+}
+
+fn traverse2(name: &str, rules: &Vec<Rule>) -> i32 {
+    let rule = rules.iter().find(|r| r.name == name).unwrap();
+    rule.has.iter().fold(1, |a, cr| {
+        let c = a + (cr.count * traverse2(&cr.name, rules));
+        c
+    })
 }
 
 fn groups() -> Vec<Vec<String>> {
