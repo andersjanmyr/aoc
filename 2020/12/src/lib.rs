3fa6c7fb0132a2a09b810884fc8defcabfd96759
Anders Janmyr <anders@janmyr.com>
2020-12-12 11:39:34 +0100

    Solved 12b

--- 2020/12/src/lib.rs
+++ 2020/12/src/lib.rs
@@ -1,127 +1,86 @@
 use std::fs::File;
 use std::io::{self, prelude::*};
 
+#[derive(Debug)]
+struct Move {
+    name: char,
+    count: i32,
+}
+
 pub fn main() {
     let ls = strings();
-    let mut grid: Vec<Vec<char>> = ls.iter().map(|s| s.chars().collect()).collect();
-    print_grid(&grid);
-
-    let mut next = simulate(&grid);
-    print_grid(&next);
-    while next != grid {
-        grid = next;
-        next = simulate(&grid);
-    }
-    print_grid(&next);
-    println!("{:?}", count(&grid));
+    let moves = get_moves(&ls);
+    let (x, y) = simulate((10, 1), &moves);
+    let dist = x.abs() + y.abs();
+    println!("{:?}", dist);
 }
 
-fn print_grid(grid: &Vec<Vec<char>>) {
-    for r in grid {
-        let s: String = r.into_iter().collect();
-        println!("{:?}", s);
-    }
-    println!("");
-}
-
-fn count(grid: &Vec<Vec<char>>) -> usize {
-    let mut count = 0;
-    for r in 0..grid.len() {
-        for c in 0..grid[r].len() {
-            if grid[r][c] == '#' {
-                count += 1;
+fn get_moves(lines: &Vec<String>) -> Vec<Move> {
+    lines
+        .iter()
+        .map(|l| {
+            let cs: Vec<char> = l.chars().collect();
+            let s: String = cs[1..].into_iter().collect();
+            Move {
+                name: cs[0],
+                count: s.parse::<i32>().unwrap(),
             }
+        })
+        .collect()
+}
+
+fn simulate(waypoint: (i32, i32), moves: &Vec<Move>) -> (i32, i32) {
+    let mut x = 0;
+    let mut y = 0;
+    let mut wx = waypoint.0;
+    let mut wy = waypoint.1;
+    for m in moves {
+        match m.name {
+            'N' => wy += m.count,
+            'S' => wy -= m.count,
+            'E' => wx += m.count,
+            'W' => wx -= m.count,
+            'R' => {
+                let p = turn_right(m.count, wx, wy);
+                wx = p.0;
+                wy = p.1;
+            }
+            'L' => {
+                let p = turn_left(m.count, wx, wy);
+                wx = p.0;
+                wy = p.1;
+            }
+            'F' => {
+                let dx = m.count * wx;
+                let dy = m.count * wy;
+                x += dx;
+                y += dy;
+            }
+            _ => panic!(""),
         }
+        println!("{:?} {:?} {:?} {:?}", x, y, wx, wy);
     }
-    count
+    (x, y)
 }
 
-fn simulate(grid: &Vec<Vec<char>>) -> Vec<Vec<char>> {
-    let mut copy = grid.to_vec();
-    for r in 0..grid.len() {
-        for c in 0..grid[r].len() {
-            let adj = adjacent(grid, r, c);
-            if grid[r][c] == 'L' && adj == 0 {
-                copy[r][c] = '#';
-            }
-            if grid[r][c] == '#' && adj >= 5 {
-                copy[r][c] = 'L';
-            }
-        }
+fn turn_right(deg: i32, wx: i32, wy: i32) -> (i32, i32) {
+    match deg {
+        0 => (wx, wy),
+        90 => (wy, -wx),
+        180 => (-wx, -wy),
+        270 => (-wy, wx),
+        _ => panic!(""),
     }
-    copy
 }
 
-fn adjacent(grid: &Vec<Vec<char>>, r: usize, c: usize) -> i32 {
-    let mut count = 0;
-    for line in adjacents(grid.len(), grid[0].len(), r, c) {
-        for (i, j) in line {
-            if grid[i][j] == '#' {
-                count += 1;
-                break;
-            }
-            if grid[i][j] == 'L' {
-                break;
-            }
-        }
+fn turn_left(deg: i32, wx: i32, wy: i32) -> (i32, i32) {
+    match deg {
+        0 => (wx, wy),
+        90 => (-wy, wx),
+        180 => (-wx, -wy),
+        270 => (wy, -wx),
+        _ => panic!(""),
     }
-    // println!("{:?} {:?} {:?}", r, c, vs);
-    count
-}
-
-fn get_coordinates(
-    r: i32,
-    c: i32,
-    rlim: i32,
-    clim: i32,
-    rdelta: i32,
-    cdelta: i32,
-) -> Vec<(usize, usize)> {
-    let mut v: Vec<(usize, usize)> = Vec::new();
-    let mut i = r as i32;
-    let mut j = c as i32;
-    while i != rlim as i32 && j != clim as i32 {
-        v.push((i as usize, j as usize));
-        i += rdelta;
-        j += cdelta;
-    }
-    v
-}
-
-fn adjacents(rlen: usize, clen: usize, r: usize, c: usize) -> Vec<Vec<(usize, usize)>> {
-    let mut vs: Vec<Vec<(usize, usize)>> = Vec::new();
-    let rmin: i32 = r as i32 - 1;
-    let cmin: i32 = c as i32 - 1;
-    let rmax = r as i32 + 1;
-    let cmax = c as i32 + 1;
-
-    vs.push(get_coordinates(rmin, cmin, -1, -1, -1, -1));
-    vs.push(get_coordinates(rmin, c as i32, -1, -1, -1, 0));
-    vs.push(get_coordinates(rmin, cmax, -1, clen as i32, -1, 1));
-    vs.push(get_coordinates(r as i32, cmin, -1, -1, 0, -1));
-    vs.push(get_coordinates(r as i32, cmax, -1, clen as i32, 0, 1));
-    vs.push(get_coordinates(rmax, cmin, rlen as i32, -1, 1, -1));
-    vs.push(get_coordinates(rmax, c as i32, rlen as i32, -1, 1, 0));
-    vs.push(get_coordinates(rmax, cmax, rlen as i32, clen as i32, 1, 1));
-
-    vs
-}
-
-fn find_first_sum(ns: &Vec<i64>, s: i64) -> (usize, usize, i64, i64) {
-    for i in 0..ns.len() {
-        for j in i + 1..ns.len() {
-            let mut v = ns[i..j].to_vec();
-            let sum = v.iter().fold(0, |a, i| a + i);
-            if sum == s {
-                v.sort();
-                return (i, j - 1, v[0], v[v.len() - 1]);
-            }
-            if sum > s {
-                break;
-            }
-        }
-    }
-    panic!("");
 }
 
 fn groups() -> Vec<Vec<String>> {
@@ -140,9 +99,9 @@ fn groups() -> Vec<Vec<String>> {
     gs
 }
 
-fn numbers() -> Vec<i64> {
+fn numbers() -> Vec<i32> {
     let lines = strings();
-    lines.iter().map(|s| s.parse::<i64>().unwrap()).collect()
+    lines.iter().map(|s| s.parse::<i32>().unwrap()).collect()
 }
 
 fn num_matrix() -> Vec<Vec<i32>> {
