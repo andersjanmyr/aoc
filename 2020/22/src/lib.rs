59c7b56c80399af8091ad9123192b151799058a4
Anders Janmyr <anders@janmyr.com>
2020-12-22 12:27:18 +0100

    22 solved

--- 2020/22/src/lib.rs
+++ 2020/22/src/lib.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::{HashSet, VecDeque};
 use std::fs::read_to_string;
 
 fn main() {
@@ -7,8 +7,7 @@ fn main() {
     let (p1, p2) = parse_input(&data);
 
     println!("{:?} {:?}", p1, p2);
-    let mut set = HashSet::new();
-    let (p1, p2) = play_game(p1, p2, &mut set);
+    let (p1, p2) = play_game(p1, p2);
     println!("{:?} {:?}", p1, p2);
     let score = if p1.len() > p2.len() {
         calc_score(p1)
@@ -18,64 +17,53 @@ fn main() {
     println!("{:?}", score);
 }
 
-fn calc_score(ps: Vec<usize>) -> usize {
-    let mut sum = 0;
+fn calc_score(ps: VecDeque<u8>) -> usize {
+    let mut sum: usize = 0;
     for i in 0..ps.len() {
         let f = (ps.len() - i) as usize;
-        sum += f * ps[i];
+        sum += f * ps[i] as usize;
     }
     sum
 }
 
-fn play_game(
-    p1: Vec<usize>,
-    p2: Vec<usize>,
-    set: &mut HashSet<(Vec<usize>, Vec<usize>)>,
-) -> (Vec<usize>, Vec<usize>) {
-    println!("Game: {:?}", set.len());
-    let mut p1 = p1;
-    let mut p2 = p2;
+fn play_game(mut p1: VecDeque<u8>, mut p2: VecDeque<u8>) -> (VecDeque<u8>, VecDeque<u8>) {
+    let mut set = HashSet::new();
     while p1.len() > 0 && p2.len() > 0 {
-        if set.contains(&(p1.clone(), p2.clone())) {
-            println!("inf");
-            return (p1.clone(), p2.clone());
+        if !set.insert((p1.clone(), p2.clone())) {
+            println!("inf {:?}", p1.len() + p2.len());
+            return (p1, p2);
         }
-        set.insert((p1.clone(), p2.clone()));
-        let c1 = p1.remove(0);
-        let c2 = p2.remove(0);
-        if c1 <= p1.len() && c2 <= p2.len() {
-            let (r1, _) = play_game(p1.clone(), p2.clone(), set);
+        let c1 = p1.pop_front().unwrap();
+        let c2 = p2.pop_front().unwrap();
+        if c1 <= p1.len() as u8 && c2 <= p2.len() as u8 {
+            let (r1, _) = play_game(p1.clone(), p2.clone());
             if r1.len() > 0 {
-                p1.push(c1);
-                p1.push(c2);
+                p1.push_back(c1);
+                p1.push_back(c2);
             } else {
-                p2.push(c2);
-                p2.push(c1);
+                p2.push_back(c2);
+                p2.push_back(c1);
             }
         } else if c1 > c2 {
-            p1.push(c1);
-            p1.push(c2);
+            p1.push_back(c1);
+            p1.push_back(c2);
         } else {
-            p2.push(c2);
-            p2.push(c1);
+            p2.push_back(c2);
+            p2.push_back(c1);
         }
     }
     (p1, p2)
 }
 
-fn parse_input(s: &str) -> (Vec<usize>, Vec<usize>) {
-    let v: Vec<Vec<usize>> = s
-        .lines()
-        .collect::<Vec<_>>()
-        .split(|&l| l == "")
-        .map(|lines| {
-            lines[1..]
-                .iter()
-                .map(|l| l.parse::<usize>().unwrap())
-                .collect()
-        })
-        .collect();
-    (v[0].clone(), v[1].clone())
+fn parse_input(s: &str) -> (VecDeque<u8>, VecDeque<u8>) {
+    let gs = s.trim().split("\n\n").collect::<Vec<&str>>();
+    (parse_cards(&gs[0]), parse_cards(&gs[1]))
+}
+
+fn parse_cards(s: &str) -> VecDeque<u8> {
+    let mut iter = s.lines();
+    iter.next();
+    iter.map(|l| l.parse::<u8>().unwrap()).collect()
 }
 
 #[cfg(test)]
